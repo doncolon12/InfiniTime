88cd802fe4f3b8b4c89ff92a5855a72089af4a32
doncolon12 <doncolon12@gmail.com>
2025-12-28 16:29:02 +0100

    Update WatchFaceCasioStyleG7710.cpp

--- src/displayapp/screens/WatchFaceCasioStyleG7710.cpp
+++ src/displayapp/screens/WatchFaceCasioStyleG7710.cpp
@@ -134,17 +134,17 @@ WatchFaceCasioStyleG7710::WatchFaceCasioStyleG7710(Controllers::DateTime& dateTi
   lv_obj_add_style(line_date, LV_LINE_PART_MAIN, &style_line);
   lv_obj_align(line_date, nullptr, LV_ALIGN_IN_TOP_RIGHT, 0, 100);
 
-  // Time: main HH:MM
+  // Main time (HH:MM)
   label_time = lv_label_create(lv_scr_act(), nullptr);
   lv_obj_set_style_local_text_color(label_time, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, color_text);
   lv_obj_set_style_local_text_font(label_time, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, font_segment115);
   lv_obj_align(label_time, lv_scr_act(), LV_ALIGN_CENTER, 0, 40);
-
-  // Small seconds label
+  
+  // Seconds (SS) to the right and slightly lower
   label_time_seconds = lv_label_create(lv_scr_act(), nullptr);
   lv_obj_set_style_local_text_color(label_time_seconds, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, color_text);
   lv_obj_set_style_local_text_font(label_time_seconds, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, font_segment40);
-  lv_obj_align(label_time_seconds, label_time, LV_ALIGN_OUT_RIGHT_MID, 5, 0);
+  lv_obj_align(label_time_seconds, label_time, LV_ALIGN_OUT_RIGHT_MID, 5, 20);  // right + lower
 
   line_time = lv_line_create(lv_scr_act(), nullptr);
   lv_line_set_points(line_time, line_time_points, 3);
